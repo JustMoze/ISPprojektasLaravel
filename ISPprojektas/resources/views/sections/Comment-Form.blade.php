e3a5509bd5b914414e90030303aa5cdfa70af93e
Aleksandr Naruševič <43404885+Aleksandras97@users.noreply.github.com>
2019-11-21 22:28:17 +0200

    Revert "FE-Atsiliepimai, Nusiskundimai ir jų pridėjimas"

--- ISPprojektas/resources/views/sections/Comment-Form.blade.php
+++ ISPprojektas/resources/views/sections/Comment-Form.blade.php
@@ -1,32 +1,15 @@
 <div class="Comment-sec container">
-  <h1>Įvertinkite kambarį!</h1>
   <div class="row">
     <div class="col-lg-6">
-      @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-      @endif
       <form method="post" action="{{ route('Comment-submit-form') }}">
         @csrf
-
-        <div class="form-group">
-          <label for="rating">Vertinimas nuo 1 iki 5</label>
-          <div class="vertinimo-iconos">
-            <i class="fas fa-fire fa-3x checked" id="1one"></i>
-            <i class="fas fa-fire fa-3x checked" id="2one"></i>
-            <i class="fas fa-fire fa-3x checked" id="3one"></i>
-            <i class="fas fa-fire fa-3x checked" id="4one"></i>
-            <i class="fas fa-fire fa-3x checked" id="5one"></i>
-          </div>
-          <input type="text" class="form-control vertinimo-laukas" name="rating" id="rating"  readonly="readonly">
+        <div class="">
+          <i class="fas fa-fire fa-3x"></i>
+          <i class="fas fa-fire fa-3x"></i>
+          <i class="fas fa-fire fa-3x"></i>
+          <i class="fas fa-fire fa-3x"></i>
+          <i class="fas fa-fire fa-3x"></i>
         </div>
-        <hr>
-        <h2>Palikite atiliepimą(nebūtina)</h2>
         <div class="form-group">
           <label for="title" >Title</label>
           <input type="text" class="form-control" name="title" id="title"  placeholder="Enter title">
@@ -40,4 +23,3 @@
     </div>
   </div>
 </div>
-<script src="{{asset('js/app.js')}}" charset="utf-8"></script>
