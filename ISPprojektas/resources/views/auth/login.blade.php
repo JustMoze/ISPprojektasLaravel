171050afe8fe06cdafa91936552b4d2677550bda
Modestas <45768923+JustMoze@users.noreply.github.com>
2019-11-30 20:30:43 +0200

    fixed registration and login view blades

--- ISPprojektas/resources/views/auth/login.blade.php
+++ ISPprojektas/resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     Prisijungti
                                 </button>
 
